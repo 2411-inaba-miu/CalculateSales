209412076c15396b6cae0bd91076f5344b58d9fb
inaba_miu <inaba.miu@alhinc.jp>
2024-11-12 16:15:02 +0900

    売り上げ集計課題の2‐2を対応中です。

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
@@ -31,28 +33,57 @@ public class CalculateSales {
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
 
-		// 支店定義ファイル読み込み処理
-		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
+			// 支店定義ファイル読み込み処理
+			if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
 			return;
-		}
+			}
 
-		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
-		//売り上げ集計課題のファイルの中にある全てのファイルを配列にする指示
-	    File[] files = new File(args[0]).listFiles();
+			// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+			//売り上げ集計課題のファイルの中にある全てのファイルを配列にする指示
 
-		//全ファイル数分の処理を繰り返し行うことを指示
-		for(int i = 0; i < files.length ; i++) {
-			 System.out.println (files[i].getName());
-		}
-		//全ファイル数分の処理を繰り返し行うことを指示
-		if(args[0].matches ("[0-9]{5}")){
-		if(args[0].matches(".+rcd$")) {
+			File[] files = new File(args[0]).listFiles();
 
-			? = new ArrayList<>();
-			rcdFiles.add(files[i]?);
-		}
+			//先にファイルの情報を格納する List(ArrayList) を宣⾔します。
+			List<File> rcdFiles = new ArrayList<>();
 
-		}
+			//全ファイル数分の処理を繰り返し行うことを指示
+			for(int i = 0; i < files.length ; i++) {
+				files[i].getName();
+				String filenames = files[i].getName();
+
+
+			//全ファイル数分の処理を繰り返し行うことを指示
+				if(filenames.matches ("[0-9]{8}.+rcd$")) {
+				rcdFiles.add(files[i]);
+				}
+			}
+
+		for(int i = 0; i < rcdFiles.size(); i++) {
+
+				BufferedReader br = null;
+
+				try {
+				File file = new File(rcdFiles);
+				FileReader fr = new FileReader(file);
+				br = new BufferedReader(fr);
+
+				} catch(IOException e) {
+				System.out.println(UNKNOWN_ERROR);
+				return false;
+				} finally {
+				// ファイルを開いている場合
+				if(br != null) {
+					try {
+						// ファイルを閉じる
+						br.close();
+					} catch(IOException e) {
+						System.out.println(UNKNOWN_ERROR);
+						return false;
+					}
+				}
+			}
+			return true;
+			}
 
 		// 支店別集計ファイル書き込み処理
 		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
@@ -73,6 +104,14 @@ public class CalculateSales {
 	private static boolean readFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
 		BufferedReader br = null;
 
+		/**ファイル型のfileという変数を置き、そこにファイルパスとファイル名を代入する。
+		 * File Reader型のfrという変数を置き、そこにfile(path, fileName)を代入する。
+		 * BufferedReader型のbrという初期化した変数を置き、そこにfr(path, fileName)を代入する。
+		 *
+		 * String型のlineという変数を宣言する。
+		 * lineという変数にBufferedReaderのreadLineメソッドで空欄ではない箇所を一行ずつ読み込む。
+		 * String[]型の変数でitemを宣言し、そこにlineのsplitメソッドを使用してカンマ区切りにした値を代入する。
+		 */
 		try {
 			File file = new File(path, fileName);
 			FileReader fr = new FileReader(file);
@@ -82,9 +121,9 @@ public class CalculateSales {
 			// 一行ずつ読み込む
 			while((line = br.readLine()) != null) {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
-				String[] items = line.split(",");
+			String[] items = line.split(",");
 
-				//Mapに追加する2つの情報を putの引数として指定します。
+			//Mapに追加する2つの情報を putの引数として指定します。
 				branchNames.put( items[0], items[1]);
 				branchSales.put( items[0], 0L);
 
